f4c7b6f248e1c2b3631a5c0568b8783c2a5ec93b
JoelWeslei <joelwezlei@gmail.com>
2025-01-14 20:35:33 -0300

    organização aquivo primeira aula js

---
+++ operadores.js
@@ -0,0 +1,18 @@
+
+
+let nomeUsuario = prompt('Qual seu nome?')
+console.log("Seu nome é :" + nomeUsuario)
+
+let datanascismentoUsuario = prompt('Qual sua data de nascimento?')
+console.log("Sua data de nascimento é :" + datanascismentoUsuario)
+
+let emailUsuario = prompt ('Qual seu email?')
+console.log("Seu email é :" + emailUsuario)
+
+let salarioUsuario = prompt ('Qual o seu salário?')
+console.log('Seu salário é: ' + salarioUsuario)
+ 
+salarioUsuario = Number(salarioUsuario)
+console.log(salarioUsuario + 1000)
+
+
---
+++ script.js
@@ -0,0 +1,33 @@
+console.log('Óla mundo!')
+console.log("Óla mundo!")
+
+
+let nome = "joel Weslei"
+
+let idade = 20
+
+let email= "joel@gmail.com"
+
+const dataNascimento = "18/09/2004"
+
+
+console.log(nome)
+console.log(idade)
+console.log(email)
+console.log(dataNascimento)
+
+let nomeUsuario = prompt('Qual seu nome?')
+console.log("Seu nome é :" + nomeUsuario)
+
+let datanascismentoUsuario = prompt('Qual sua data de nascimento?')
+console.log("Sua data de nascimento é :" + datanascismentoUsuario)
+
+let emailUsuario = prompt ('Qual seu email?')
+console.log("Seu email é :" + emailUsuario)
+
+let salarioUsuario = prompt ('Qual o seu salário?')
+console.log('Seu salário é: ' + salarioUsuario)
+ 
+salarioUsuario = Number(salarioUsuario)
+console.log(salarioUsuario + 1000)
+
